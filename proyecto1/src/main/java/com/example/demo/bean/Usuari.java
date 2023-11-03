b8028893acff4844d047186badeec0003dbc9340
Andres <cf22andres.zambrano@iesjoandaustria.org>
2023-11-03 15:17:04 +0100

    Division de clases simplificada para mayor claridad

--- proyecto1/src/main/java/com/example/demo/bean/Usuari.java
+++ proyecto1/src/main/java/com/example/demo/bean/Usuari.java
@@ -2,4 +2,56 @@ package com.example.demo.bean;
 
 public class Usuari {
 
+    private int id;
+    private String nom;
+    private String cognom;
+    private enum Tipus {ADMIN, ALUMNE, EMPRESA};
+    private Tipus tipusUsuari;
+
+    public Usuari(int id, String nom, String cognom, Tipus tipus) {
+        this.id = id;
+        this.nom = nom;
+        this.cognom = cognom;
+        this.tipusUsuari = tipus;
+    }
+
+    public Usuari() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getCognom() {
+        return cognom;
+    }
+
+    public Tipus getTipusUsuari() {
+        return tipusUsuari;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setCognom(String cognom) {
+        this.cognom = cognom;
+    }
+
+    public void setTipusUsuari(Tipus tipusUsuari) {
+        this.tipusUsuari = tipusUsuari;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuari{" + "id=" + id + ", nom=" + nom + ", cognom=" + cognom + ", tipusUsuari=" + tipusUsuari + '}';
+    }
 }
--- proyecto1/src/main/java/com/example/demo/repository/BaseDatos.java
+++ proyecto1/src/main/java/com/example/demo/repository/BaseDatos.java
@@ -1,5 +1,14 @@
 package com.example.demo.repository;
 
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class BaseDatos {
+    
 
 }
