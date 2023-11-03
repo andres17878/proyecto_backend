145551b1dd356cc2f4adf6e1683ad4df57deb54e
Andres <cf22andres.zambrano@iesjoandaustria.org>
2023-11-03 15:31:28 +0100

    Base de dades començada

--- proyecto1/src/main/java/com/example/demo/repository/BaseDatos.java
+++ proyecto1/src/main/java/com/example/demo/repository/BaseDatos.java
@@ -1,14 +1,29 @@
 package com.example.demo.repository;
 
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+
+import com.example.demo.bean.Usuari;
 
 public class BaseDatos {
-    
+
+    private Connection conexion;
+
+    public BaseDatos() {
+        try{
+            Class.forName("org.h2.Driver");
+            String conex = "jdbc:h2:mem:borsa_de_treball;DB_CLOSE_DELAY=-1";
+            this.conexion = DriverManager.getConnection(conex, "sa", "");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Database connection failed");
+            e.printStackTrace();
+        }
+    }
+
+    public void consultaEmpresas(){
+        // Check from the h2 database the table "EMPRESA" and its content
+        
+    }
+
+
 
 }
