03b7d78996c8e09ac80f22f7fc1200b15fb40094
Andres <cf22andres.zambrano@iesjoandaustria.org>
2023-11-22 15:57:45 +0100

    Import innecesario quitado

--- demo/src/test/java/com/example/demo/BorsaTest.java
+++ demo/src/test/java/com/example/demo/BorsaTest.java
@@ -1,8 +1,6 @@
 package com.example.demo;
 
 
-import java.util.ArrayList;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
